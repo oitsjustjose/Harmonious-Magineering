e481f8d5a1c9b0b2b5abe9957ceb616f825860fb
oitsjustjose <stovallj1995@gmail.com>
2023-11-19 10:41:26 -0600

    Tweaks

--- kubejs/server_scripts/1_Magic/4_Embers.js
+++ kubejs/server_scripts/1_Magic/4_Embers.js
@@ -1,3 +1,20 @@
 ServerEvents.recipes(event => {
-  // event.remove({input: 'embers:tinker_hammer', output: '#forge:plates'});
+  // Gate the Tinker Hammer behind Hellforged Ingots from BloodMagic
+  event.remove({output: 'embers:tinker_hammer'});
+  event.shaped('embers:tinker_hammer', ['LDL', 'LSL', ' S '], {
+    L: '#forge:ingots/lead',
+    D: '#forge:ingots/hellforged',
+    S: '#forge:rods/wooden',
+  });
+
+  [
+    Item.of('embers:silver_hoe'),
+    Item.of('embers:silver_axe'),
+    Item.of('embers:silver_pickaxe'),
+    Item.of('embers:silver_shovel'),
+    Item.of('embers:silver_sword'),
+  ].forEach(tool => {
+    event.remove({input: tool});
+    event.remove({output: tool});
+  });
 });
