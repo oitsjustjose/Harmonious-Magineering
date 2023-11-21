3706eb8218bfefdeabfc9c27b4fc7f2b34be83c7
oitsjustjose <stovallj1995@gmail.com>
2023-11-21 08:40:56 -0600

    Add mod name to every tooltip when pressing shift

--- kubejs/client_scripts/Tooltips.js
+++ kubejs/client_scripts/Tooltips.js
@@ -6,4 +6,17 @@ ItemEvents.tooltip(event => {
       tooltips.add(Text.darkGray('[Press Shift]'));
     }
   });
+
+  event.addAdvanced(Ingredient.all, (stack, advanced, tooltips) => {
+    if (stack.getMod() === 'minecraft') return;
+    if (event.isShift()) {
+      let Modid = stack
+        .getMod()
+        .split('_')
+        .map(x => x.substring(0, 1).toUpperCase() + x.substring(1))
+        .join(' ');
+
+      tooltips.add(Text.blue(`Added by ${Modid}`));
+    }
+  });
 });
