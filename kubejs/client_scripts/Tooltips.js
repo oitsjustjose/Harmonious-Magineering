f17e181dfd6ffab10aa948ea483385ee3d830996
oitsjustjose <stovallj1995@gmail.com>
2023-11-22 20:49:32 -0600

    Tooltips use loc now

--- kubejs/client_scripts/Tooltips.js
+++ kubejs/client_scripts/Tooltips.js
@@ -1,22 +1,16 @@
 ItemEvents.tooltip(event => {
   event.addAdvanced('enderio:dark_steel_ingot', (stack, advanced, tooltips) => {
     if (event.isShift()) {
-      tooltips.add(Text.gray('Can be found in various chests while out exploring!'));
+      tooltips.add(Text.gray(Text.translate('tooltip.kubejs.dark_steel_ingot')));
     } else {
-      tooltips.add(Text.darkGray('[Press Shift]'));
+      tooltips.add(Text.darkGray(Text.translate('tooltip.kubejs.shift')));
     }
   });
 
   event.addAdvanced(Ingredient.all, (stack, advanced, tooltips) => {
-    if (stack.getMod() === 'minecraft') return;
     if (event.isShift()) {
-      let Modid = stack
-        .getMod()
-        .split('_')
-        .map(x => x.substring(0, 1).toUpperCase() + x.substring(1))
-        .join(' ');
-
-      tooltips.add(Text.blue(`Added by ${Modid}`));
+      const ModName = Platform.getMods()[stack.getMod()].getName();
+      tooltips.add(Text.blue(Text.translate('tooltip.kubejs.modid', ModName)));
     }
   });
 });
