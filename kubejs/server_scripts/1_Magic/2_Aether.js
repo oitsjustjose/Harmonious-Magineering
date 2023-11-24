f9812a306ce445e36b56e03dc0e6e267d77589ce
oitsjustjose <stovallj1995@gmail.com>
2023-11-24 08:46:39 -0600

    Finally add Aether Portal recipe

--- kubejs/server_scripts/1_Magic/2_Aether.js
+++ kubejs/server_scripts/1_Magic/2_Aether.js
@@ -1,7 +1,13 @@
 ServerEvents.recipes(event => {
-  // event.custom({
-  //   result: {
-  //     item: "aether:aether_portal_frame"
-  //   }
-  // })
+  event.custom({
+    type: 'eidolon:worktable',
+    pattern: ['glg', 'lhl', 'glg'],
+    reagents: 'gggg',
+    key: {
+      g: {item: 'minecraft:glowstone'},
+      h: {item: 'eidolon:holy_symbol'},
+      l: {item: 'minecraft:ice'},
+    },
+    result: {item: 'aether:aether_portal_frame'},
+  });
 });
