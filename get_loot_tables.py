c73cdd9bdb154051ac6035304b547c015d3fd7ab
oitsjustjose <stovallj1995@gmail.com>
2023-12-09 20:22:54 -0600

    Ok this is *actually* helpful -_-

--- get_loot_tables.py
+++ get_loot_tables.py
@@ -1,30 +1,38 @@
 import zipfile
+import json
 import os
 
-candidates = []
+size = os.get_terminal_size() 
 
 for file in os.listdir("./mods"):
     if not file.endswith(".jar"):
         continue
 
     with zipfile.ZipFile(f"./mods/{file}", "r") as zf:
-        candidates += list(
-            map(
-                lambda x: x.filename, 
-                filter(
-                    lambda x: "loot_tables" in x.filename and "entities" in x.filename and ".json" in x.filename, 
-                    zf.infolist()
-                )
-            )
-        )
-    
-for cand in candidates:
-    parts = cand.replace(".json", "").split("/")
-    modid = parts[1]
-    path = parts[-1]
-    if modid == "minecraft":
-        print(f"{modid}:{path} ({cand})")
-    else:
-        print(f"{modid}:{path}")
+        for file in zf.infolist():
+            filename: str = file.filename
+            if not ("loot_tables" in filename and "entities" in filename and filename.endswith(".json")):
+                continue
+            with zf.open(filename, "r") as fh:
+                data = json.loads(fh.read())
 
-        
+            parts = filename.replace(".json", "").split("/")
+            namespace = parts[1]
+            path = parts[-1]
+
+            drops = []
+
+            if "pools" not in data:
+                continue
+            for pool in data["pools"]:
+                if "entries" not in pool:
+                    continue
+                for entry in filter(lambda x: "type" in x and x["type"] == "minecraft:item", pool["entries"]):
+                    if "name" not in entry:
+                        continue
+                    drops.append(entry["name"])
+            
+
+            print(f"{namespace}:{path}\n")
+            print("\n".join(drops))
+            print("_" * size.columns)
