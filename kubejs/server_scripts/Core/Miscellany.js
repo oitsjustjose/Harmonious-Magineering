9b080ba36d6a079016d523e529744441d9f287b9
oitsjustjose <stovallj1995@gmail.com>
2024-01-07 14:11:40 -0600

    Create Waxed Copper recipes

--- kubejs/server_scripts/Core/Miscellany.js
+++ kubejs/server_scripts/Core/Miscellany.js
@@ -8,6 +8,57 @@ ServerEvents.recipes(event => {
     });
   };
 
+  /* Create's Waxed Copper can't be crafted into corresponding stairs/slabs, but vanilla's can... */
+  const createWaxedCopper = () => {
+    [
+      {
+        block: 'create:waxed_copper_shingles',
+        stair: 'create:waxed_copper_shingle_stairs',
+        slab: 'create:waxed_copper_shingle_slab',
+      },
+      {
+        block: 'create:waxed_copper_tiles',
+        stair: 'create:waxed_copper_tile_stairs',
+        slab: 'create:waxed_copper_tile_slab',
+      },
+      {
+        block: 'create:waxed_exposed_copper_shingles',
+        stair: 'create:waxed_exposed_copper_shingle_stairs',
+        slab: 'create:waxed_exposed_copper_shingle_slab',
+      },
+      {
+        block: 'create:waxed_exposed_copper_tiles',
+        stair: 'create:waxed_exposed_copper_tile_stairs',
+        slab: 'create:waxed_exposed_copper_tile_slab',
+      },
+      {
+        block: 'create:waxed_oxidized_copper_shingles',
+        stair: 'create:waxed_oxidized_copper_shingle_stairs',
+        slab: 'create:waxed_oxidized_copper_shingle_slab',
+      },
+      {
+        block: 'create:waxed_oxidized_copper_tiles',
+        stair: 'create:waxed_oxidized_copper_tile_stairs',
+        slab: 'create:waxed_oxidized_copper_tile_slab',
+      },
+      {
+        block: 'create:waxed_weathered_copper_shingles',
+        stair: 'create:waxed_weathered_copper_shingle_stairs',
+        slab: 'create:waxed_weathered_copper_shingle_slab',
+      },
+      {
+        block: 'create:waxed_weathered_copper_tiles',
+        stair: 'create:waxed_weathered_copper_tile_stairs',
+        slab: 'create:waxed_weathered_copper_tile_slab',
+      },
+    ].forEach(x => {
+      event.shaped(Item.of(x.stair, 4), ['B  ', 'BB ', 'BBB'], {B: x.block});
+      event.shaped(Item.of(x.slab, 6), ['BBB'], {B: x.block});
+      event.stonecutting(Item.of(x.slab, 2), x.block);
+      event.stonecutting(Item.of(x.stair), x.block);
+    });
+  };
+
   const supplementaries = () => {
     event.remove({output: 'supplementaries:sack'});
   };
@@ -18,9 +69,12 @@ ServerEvents.recipes(event => {
     W: 'pneumaticcraft:capacitor',
   });
 
+  event.shapeless(Item.of('minecraft:bone_meal', 4), ['eidolon:imbued_bones']);
+  event.shapeless(Item.of('minecraft:clay_ball', 4), ['minecraft:clay']);
+
   event.remove({mod: 'ftbquests'});
   event.remove({output: 'minecraft:saddle'});
   event.remove('aether:swet_slime_block');
 
-  [coldsweat, supplementaries].forEach(module => module());
+  [coldsweat, createWaxedCopper, supplementaries].forEach(module => module());
 });
