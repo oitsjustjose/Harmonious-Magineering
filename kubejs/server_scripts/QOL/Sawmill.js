b4f72dc47c377ee31ce64d3dce796a568a4fa8f5
oitsjustjose <stovallj1995@gmail.com>
2024-02-12 23:25:56 -0600

    Fix typo

--- kubejs/server_scripts/QOL/Sawmill.js
+++ kubejs/server_scripts/QOL/Sawmill.js
@@ -185,7 +185,7 @@ ServerEvents.recipes(event => {
       const logTag = Ingredient.of(`#${mod}:${wood}_logs`);
       sawmillPlanking(Item.of(plank, 6), justUnstripped(logTag), justStripped(logTag));
     } else {
-      console.warn(`Plank for mod=${mod}, wood =${wood} was not found :(`);
+      console.warn(`Plank for mod=${mod}, wood=${wood} was not found :(`);
       return;
     }
 
@@ -193,14 +193,14 @@ ServerEvents.recipes(event => {
     if (slab) {
       sawmillSlabbing(Item.of(slab, 2), Item.of(plank));
     } else {
-      console.warn(`Slab for mod=${mod}, wood =${wood} was not found :(`);
+      console.warn(`Slab for mod=${mod}, wood=${wood} was not found :(`);
     }
 
     const shelf = findShelf(variant.mod, variant.wood);
     if (shelf) {
       sawmillShelving(Item.of(plank, 4), Item.of(shelf));
     } else {
-      console.warn(`Bookshelf for mod=${mod}, wood =${wood} was not found :(`);
+      console.warn(`Bookshelf for mod=${mod}, wood=${wood} was not found :(`);
     }
   });
 });
