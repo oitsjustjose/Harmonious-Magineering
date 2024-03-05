13bd04dba9f5e0b368fc5191570877266143007b
oitsjustjose <stovallj1995@gmail.com>
2024-03-04 22:19:52 -0600

    Restore changes from Expanded Storage removal

--- kubejs/server_scripts/QOL/Storage.js
+++ kubejs/server_scripts/QOL/Storage.js
@@ -14,8 +14,23 @@ ServerEvents.recipes(event => {
     });
   };
 
+  // Replace minecraft:chest with tags
+  event.replaceInput({}, 'minecraft:chest', '#forge:chests/wooden');
+  event.replaceOutput({}, 'minecraft:chest', 'expandedstorage:wood_chest');
+
+  // Remove the recipe for the vanilla chest, replace w/ a swap recipe
+  event.remove({ output: 'minecraft:chest' });
+  event.shapeless('minecraft:chest', ['expandedstorage:wood_chest']);
+
+  // Better recipe for the Full-block wooden chest
+  event.remove({ output: 'expandedstorage:old_wood_chest' });
+  event.shaped('expandedstorage:old_wood_chest', ['PPP', 'PCP', 'PPP'], {
+    P: '#minecraft:planks',
+    C: 'expandedstorage:wood_chest',
+  });
+
   // 4 chests from logs
-  event.shaped('4x minecraft:chest', ['LLL', 'L L', 'LLL'], { L: '#minecraft:logs' });
+  event.shaped(Item.of('expandedstorage:wood_chest', 4), ['LLL', 'L L', 'LLL'], { L: '#minecraft:logs' });
 
   // Bundles!
   event.shaped('minecraft:bundle', [' S ', 'L L', ' L '], {
@@ -183,4 +198,11 @@ ServerEvents.recipes(event => {
 
   event.remove({ output: 'storagedrawers:void_upgrade' });
   event.shapeless('storagedrawers:void_upgrade', ['trashcans:item_trash_can', 'storagedrawers:upgrade_template']);
+});
+
+LootJS.modifiers(event => {
+  /* Replace vanilla chests with Expanded Storage's */
+  event.addEntityLootModifier('aether:mimic').replaceLoot('minecraft:chest', 'expandedstorage:wood_chest');
+  event.addBlockLootModifier(/.*/).replaceLoot('minecraft:chest', 'expandedstorage:wood_chest');
+  event.addLootTableModifier(/.*/).replaceLoot('minecraft:chest', 'expandedstorage:wood_chest');
 });
