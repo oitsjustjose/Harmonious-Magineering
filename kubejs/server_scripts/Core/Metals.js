d62f1a5603d0bd8bae4524a418e541a3610a032d
oitsjustjose <stovallj1995@gmail.com>
2025-01-24 19:42:43 -0600

    fix recipe id registry

--- kubejs/server_scripts/Core/Metals.js
+++ kubejs/server_scripts/Core/Metals.js
@@ -1,6 +1,8 @@
 // priority: 1
 
 ServerEvents.recipes(event => {
+  const normalize = id => id.replace(':', '_');
+
   const automationCompat = () => {
     /* Remove all infinite metal recipes from Create */
     event.remove('create:splashing/gravel');
@@ -23,20 +25,20 @@ ServerEvents.recipes(event => {
       const metalData = global.Metals[metal];
 
       /* Enrich 1x Ore into 2x Dust */
-      event.custom({type: 'mekanism:enriching', input: {ingredient: {tag: `forge:ores/${metal}`}}, output: {item: metalData.dust, count: 2}});
+      event.custom({ type: 'mekanism:enriching', input: { ingredient: { tag: `forge:ores/${metal}` } }, output: { item: metalData.dust, count: 2 } });
       /* Crush 1x Ingot into 1x Dust */
-      event.custom({type: 'mekanism:crushing', input: {ingredient: {item: metalData.ingot}}, output: {item: metalData.dust}});
+      event.custom({ type: 'mekanism:crushing', input: { ingredient: { item: metalData.ingot } }, output: { item: metalData.dust } });
       /* Enrich 1x Raw Ore Block into 12x Dust */
       event.custom({
         type: 'mekanism:enriching',
-        input: {ingredient: {tag: `forge:storage_blocks/raw_${metal}`}},
-        output: {item: metalData.dust, count: 12},
+        input: { ingredient: { tag: `forge:storage_blocks/raw_${metal}` } },
+        output: { item: metalData.dust, count: 12 },
       });
       /* Enrich 3x Raw Ore into 4x Dust */
       event.custom({
         type: 'mekanism:enriching',
-        input: {ingredient: {tag: `forge:raw_materials/${metal}`}, amount: 3},
-        output: {item: metalData.dust, count: 4},
+        input: { ingredient: { tag: `forge:raw_materials/${metal}` }, amount: 3 },
+        output: { item: metalData.dust, count: 4 },
       });
     });
 
@@ -48,12 +50,12 @@ ServerEvents.recipes(event => {
   const dustSmelting = () => {
     Object.keys(global.Metals).forEach(mat => {
       const entry = global.Metals[mat];
-      const idBase = `${entry.ingot.replace(':', '_')}_from_${entry.dust.replace(':', '_')}`;
+      const idBase = `${normalize(entry.ingot)}_from_${normalize(entry.dust)}`;
 
-      event.remove({type: 'minecraft:blasting', input: entry.dust});
-      event.remove({type: 'minecraft:blasting', output: entry.ingot});
-      event.remove({type: 'minecraft:smelting', input: entry.dust});
-      event.remove({type: 'minecraft:smelting', output: entry.ingot});
+      event.remove({ type: 'minecraft:blasting', input: entry.dust });
+      event.remove({ type: 'minecraft:blasting', output: entry.ingot });
+      event.remove({ type: 'minecraft:smelting', input: entry.dust });
+      event.remove({ type: 'minecraft:smelting', output: entry.ingot });
 
       event.smelting(entry.ingot, entry.dust).xp(0.33).id(`kubejs:${idBase}_smelting`);
       event.blasting(entry.ingot, entry.dust).xp(0.33).id(`kubejs:${idBase}_blasting`);
@@ -71,18 +73,18 @@ ServerEvents.recipes(event => {
       };
 
       types.forEach(type => {
-        event.remove({type: type, input: tags.ingot, output: tags.nugget});
-        event.remove({type: type, input: tags.block, output: tags.ingot});
-        event.remove({type: type, input: tags.ingot, output: tags.block});
-        event.remove({type: type, input: tags.nugget, output: tags.ingot});
-        event.remove({type: type, input: metal.ingot, output: metal.nugget});
-        event.remove({type: type, input: metal.block, output: metal.ingot});
-        event.remove({type: type, input: metal.ingot, output: metal.block});
-        event.remove({type: type, input: metal.nugget, output: metal.ingot});
+        event.remove({ type: type, input: tags.ingot, output: tags.nugget });
+        event.remove({ type: type, input: tags.block, output: tags.ingot });
+        event.remove({ type: type, input: tags.ingot, output: tags.block });
+        event.remove({ type: type, input: tags.nugget, output: tags.ingot });
+        event.remove({ type: type, input: metal.ingot, output: metal.nugget });
+        event.remove({ type: type, input: metal.block, output: metal.ingot });
+        event.remove({ type: type, input: metal.ingot, output: metal.block });
+        event.remove({ type: type, input: metal.nugget, output: metal.ingot });
       });
 
-      event.shaped(metal.ingot, ['NNN', 'NNN', 'NNN'], {N: tags.nugget});
-      event.shaped(metal.block, ['NNN', 'NNN', 'NNN'], {N: tags.ingot});
+      event.shaped(metal.ingot, ['NNN', 'NNN', 'NNN'], { N: tags.nugget });
+      event.shaped(metal.block, ['NNN', 'NNN', 'NNN'], { N: tags.ingot });
       event.shapeless(Item.of(metal.nugget, 9), [tags.ingot]);
       event.shapeless(Item.of(metal.ingot, 9), [tags.block]);
     });
@@ -128,51 +130,42 @@ ServerEvents.recipes(event => {
   };
 
   const oreSmelting = () => {
-    const generateId = (output, input) => {
-      const inputConv = input.replace(/:/g, '_');
-      const outputConv = output.replace(/:/g, '_');
-      return `kubejs:${outputConv}_from_${inputConv}`;
-    };
-
     Object.keys(global.Metals).forEach(mat => {
       let metal = global.Metals[mat];
 
       // Remove raw ore -> ingot blasting/smelting
-      event.remove({type: 'minecraft:smelting', input: `#forge:raw_materials/${mat}`, output: `#forge:ingots/${mat}`});
-      event.remove({type: 'minecraft:blasting', input: `#forge:raw_materials/${mat}`, output: `#forge:ingots/${mat}`});
+      event.remove({ type: 'minecraft:smelting', input: `#forge:raw_materials/${mat}`, output: `#forge:ingots/${mat}` });
+      event.remove({ type: 'minecraft:blasting', input: `#forge:raw_materials/${mat}`, output: `#forge:ingots/${mat}` });
       // Remove ore -> ingot blasting/smelting (just in case)
-      event.remove({type: 'minecraft:smelting', input: `#forge:ores/${mat}`, output: `#forge:ingots/${mat}`});
-      event.remove({type: 'minecraft:blasting', input: `#forge:ores/${mat}`, output: `#forge:ingots/${mat}`});
+      event.remove({ type: 'minecraft:smelting', input: `#forge:ores/${mat}`, output: `#forge:ingots/${mat}` });
+      event.remove({ type: 'minecraft:blasting', input: `#forge:ores/${mat}`, output: `#forge:ingots/${mat}` });
 
       // Raw ore smelting/blasting -> 3/4 nuggets
-      let id = generateId(metal.nugget, `raw_${mat}`); // Use let because const persists between iterations in the loop even though it's not supposed to
-      event.smelting(Item.of(metal.nugget, 3), `#forge:raw_materials/${mat}`).xp(0.3).id(`${id}_smelting`);
-      event.blasting(Item.of(metal.nugget, 4), `#forge:raw_materials/${mat}`).xp(0.4).id(`${id}_blasting`);
+      event.smelting(Item.of(metal.nugget, 3), `#forge:raw_materials/${mat}`).xp(0.3).id(`kubejs:three_${normalize(metal.nugget)}_from_raw_${mat}_smelting`);
+      event.blasting(Item.of(metal.nugget, 4), `#forge:raw_materials/${mat}`).xp(0.4).id(`kubejs:four_${normalize(metal.nugget)}_from_raw_${mat}_blasting`);
 
       // Whole ore smelting/blasting -> 1 ingot, always
-      id = generateId(metal.ingot, `${mat}_ore`);
-      event.smelting(metal.ingot, `#forge:ores/${mat}`).xp(1.0).id(`${id}_smelting`);
-      event.blasting(metal.ingot, `#forge:ores/${mat}`).xp(1.0).id(`${id}_blasting`);
+      event.smelting(metal.ingot, `#forge:ores/${mat}`).xp(1.0).id(`kubejs:${normalize(metal.ingot)}_from_${mat}_ore_smelting`);
+      event.blasting(metal.ingot, `#forge:ores/${mat}`).xp(1.0).id(`kubejs:${normalize(metal.ingot)}_from_${mat}_ore_blasting`);
 
       // Crushed Ore Smelting
-      id = generateId(metal.ingot, `crushed_${mat}`);
-      event.smelting(metal.ingot, metal.crushed).xp(0.3).id(`${id}_smelting`);
-      event.blasting(metal.ingot, metal.crushed).xp(0.3).id(`${id}_blasting`);
+      event.smelting(metal.ingot, metal.crushed).xp(0.3).id(`kubejs:${normalize(metal.ingot)}_from_${normalize(metal.crushed)}_smelting`);
+      event.blasting(metal.ingot, metal.crushed).xp(0.3).id(`kubejs:${normalize(metal.ingot)}_from_${normalize(metal.crushed)}_blasting`);
     });
   };
 
   /* Some crushed metals don't have a "complement" item when washed. Let's fix that :) */
   const oreWashing = () => {
     /* Uranium devolves into Lead, so let's make it output some lead */
-    event.remove({type: 'create:splashing', input: 'create:crushed_raw_uranium'});
+    event.remove({ type: 'create:splashing', input: 'create:crushed_raw_uranium' });
     event.recipes.create.splashing(
       [Item.of('mekanism:nugget_uranium', 9), Item.of('eidolon:lead_nugget').withChance(0.1)],
       'create:crushed_raw_uranium'
     );
 
     /* Silver & Lead often form as Galena, so they can have a chance of providing each other */
-    event.remove({type: 'create:splashing', input: 'create:crushed_raw_lead'});
-    event.remove({type: 'create:splashing', input: 'create:crushed_raw_silver'});
+    event.remove({ type: 'create:splashing', input: 'create:crushed_raw_lead' });
+    event.remove({ type: 'create:splashing', input: 'create:crushed_raw_silver' });
 
     event.recipes.create.splashing([Item.of('eidolon:lead_nugget', 9), Item.of('eidolon:silver_nugget').withChance(0.05)], 'create:crushed_raw_lead');
     event.recipes.create.splashing(
@@ -181,18 +174,18 @@ ServerEvents.recipes(event => {
     );
 
     /* Nickel often forms as Limonite, so it can have trace amounts of Iron */
-    event.remove({type: 'create:splashing', input: 'create:crushed_raw_nickel'});
+    event.remove({ type: 'create:splashing', input: 'create:crushed_raw_nickel' });
     event.recipes.create.splashing(
       [Item.of('immersiveengineering:nugget_nickel', 9), Item.of('minecraft:iron_nugget').withChance(0.1)],
       'create:crushed_raw_nickel'
     );
 
     /* Tin often forms in veins with Fluorite, so that works here */
-    event.remove({type: 'create:splashing', input: 'create:crushed_raw_tin'});
+    event.remove({ type: 'create:splashing', input: 'create:crushed_raw_tin' });
     event.recipes.create.splashing([Item.of('mekanism:nugget_tin', 9), Item.of('mekanism:fluorite_gem').withChance(0.1)], 'create:crushed_raw_tin');
 
     /* Aluminum often forms with Clay, Salt & Silicon, so that works too :) */
-    event.remove({type: 'create:splashing', input: 'create:crushed_raw_aluminum'});
+    event.remove({ type: 'create:splashing', input: 'create:crushed_raw_aluminum' });
     event.recipes.create.splashing(
       [Item.of('immersiveengineering:nugget_aluminum', 9), Item.of('mekanism:salt').withChance(0.1)],
       'create:crushed_raw_aluminum'
@@ -201,8 +194,8 @@ ServerEvents.recipes(event => {
 
   const plateCompat = () => {
     // Remove recipes that let you plate/crush metals with the IE hammer
-    event.remove({input: 'immersiveengineering:hammer', output: '#forge:dusts'});
-    event.remove({input: 'immersiveengineering:hammer', output: '#forge:plates'});
+    event.remove({ input: 'immersiveengineering:hammer', output: '#forge:dusts' });
+    event.remove({ input: 'immersiveengineering:hammer', output: '#forge:plates' });
 
     /**
      * @param {Internal.Ingredient} input
@@ -255,13 +248,13 @@ ServerEvents.recipes(event => {
   const silverAndLead = () => {
     // Replace all Silver & Lead inputs to prioritize Ember's -- this is because of my weird preferences -_-
     [
-      {replace: ['immersiveengineering:ore_lead', 'mekanism:lead_ore', 'eidolon:lead_ore'], with: 'embers:lead_ore'},
+      { replace: ['immersiveengineering:ore_lead', 'mekanism:lead_ore', 'eidolon:lead_ore'], with: 'embers:lead_ore' },
       {
         replace: ['immersiveengineering:deepslate_ore_lead', 'mekanism:deepslate_lead_ore', 'eidolon:deep_lead_ore'],
         with: 'embers:deepslate_lead_ore',
       },
-      {replace: ['immersiveengineering:ore_silver', 'eidolon:silver_ore'], with: 'embers:silver_ore'},
-      {replace: ['immersiveengineering:deepslate_ore_silver', 'eidolon:deep_silver_ore'], with: 'embers:deepslate_silver_ore'},
+      { replace: ['immersiveengineering:ore_silver', 'eidolon:silver_ore'], with: 'embers:silver_ore' },
+      { replace: ['immersiveengineering:deepslate_ore_silver', 'eidolon:deep_silver_ore'], with: 'embers:deepslate_silver_ore' },
     ].forEach(x => {
       x.replace.forEach(y => {
         event.replaceInput({}, y, x.with);
